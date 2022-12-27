cf3a959aa5af21ebc2a82c948ba008d894c2c9aa
zayid <sayidka843@gmail.com>
2022-12-27 22:26:23 +0600

    resolved the expense-express task

--- App.js
+++ App.js
@@ -12,16 +12,40 @@ const expense = [
 ]
 
  
+// just get one expense with that specific id passed
+app.get("/expense/:id",(req,res)=>{
+    const index = expense.findIndex(expense=> expense.id == req.params.id)
+
+    const getSpecificExp= {
+          id:req.params.id ,
+          description: expense[index].description ,
+          amount:expense[index].amount 
+    }
+   
+    console.log(getSpecificExp)
+    res.status(200).json({Message: getSpecificExp})
+    
+})
+
+
+
 
 // get the list of expense
 app.get("/expense",(req,res)=>{
     res.status(200).json({found:expense.length, list:expense})
 })
 
+
+
 // added new item in expense list
 app.post("/expense",(req,res)=>{
-    // "ternery if" that finds the last id of expense list , If list is empty the default id would be 1
-     let id= expense[expense.length-1].id+1 ? id= expense[expense.length-1].id+1 : 1;
+    // if operation that finds the last id of expense list , If list is empty the default id would be 1
+    let id;
+    if(id >= 0 ){
+        id= expense[expense.length-1].id+1  
+     }else{
+      id =1;
+     }
 
     expense.push({id:id ,description:req.body.description ,amount:req.body.amount})
     res.status(201).json({message:expense})
@@ -39,6 +63,7 @@ app.delete("/expense/:id",(req,res)=>{
 })
 
 
+
 // return the total amount of all expenses
 app.get("/expense/:total",(req,res)=>{
     let total = 0;
@@ -51,35 +76,23 @@ app.get("/expense/:total",(req,res)=>{
     
 })
 
+
+
 // we edit the specific expense and the body should include description or amount or both
 app.put("/expense/:id",(req,res)=>{
-    const index = expense.findIndex(expense=> expense.amount == req.params.id)
+    const index = expense.findIndex(expense=> expense.id == req.params.id)
 
-    const updatedExpense= {
-          id:req.params.id ,
-          description:req.body.description,
-          amount:req.body.amount
-    }
-    expense.splice(index,1,updatedExpense)
+    expense.splice(index,1,{
+        id: parseInt(req.params.id ),
+        description:req.body.description,
+        amount:req.body.amount
+  })
     
     res.status(200).json({editedMessage:expense})
     
 })
 
-// just get one expense with that specific id passed
-app.get("/expense/:id",(req,res)=>{
-    const index = expense.findIndex(expense=> expense.amount == req.params.id)
 
-    const getSpecificExp= {
-          id:req.params.id ,
-          description: expense[index].description ,
-          amount:expense[index].amount 
-    }
-   
-    console.log(getSpecificExp)
-    res.status(200).json({Message: getSpecificExp})
-    
-})
 
 //listening the port 
 app.listen(port,()=>{
